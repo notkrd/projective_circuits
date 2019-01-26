2869a874c273869cf59983784dd545814fab8d25
Qfwfq <notkrd@gmail.com>
2019-01-26 14:37:10 -0800

    Initialized as mix module

---
+++ lib/projective_circuits.ex
@@ -0,0 +1,18 @@
+defmodule PROJECTIVE_CIRCUITS do
+  @moduledoc """
+  Documentation for PROJECTIVE_CIRCUITS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PROJECTIVE_CIRCUITS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/projective_circuits_test.exs
@@ -0,0 +1,8 @@
+defmodule PROJECTIVE_CIRCUITSTest do
+  use ExUnit.Case
+  doctest PROJECTIVE_CIRCUITS
+
+  test "greets the world" do
+    assert PROJECTIVE_CIRCUITS.hello() == :world
+  end
+end
